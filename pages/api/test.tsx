b4292eb34df3102de630568d95f9182d42f1bf02
FelixTellmann <felix@tellmann.co.za>
2020-12-15 20:45:45 +0200

    Added Blog Post

--- pages/api/test.tsx
+++ pages/api/test.tsx
@@ -2,8 +2,9 @@ import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  
-  const getData = async (url, version = "", keys?: string[]): Promise<unknown[]> => {
+  const date = new Date();
+  date.setMonth(date.getMonth() - 3);
+  const getData = async (url: string, version = "", keys?: string[]): Promise<unknown[]> => {
     try {
       const response = await axios({
         method: "get",
@@ -15,10 +16,10 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
           "Cookie": "rguserid=b2b95383-16dd-4132-a3d2-f53bdec946bb; rguuid=true; rgisanonymous=true"
         }
       });
-    
+      
       let { data } = response.data;
       console.log(response.data.version.max);
-    
+      
       if (keys) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         data = data.map(x => {
@@ -30,11 +31,11 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
         });
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       }
-  
+      
       if (version < response.data.version.max) {
         data = [...data, ...await getData(url, String(+response.data.version.max + 1), keys)];
       }
-  
+      
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       return data;
     } catch (err) {
@@ -43,16 +44,21 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
     return [];
   };
   
-  const tags = (await getData(`https://kidsliving.vendhq.com/api/2.0/tags?after=`, "0", ["name"]));
+  /* const tags = (await getData(`https://kidsliving.vendhq.com/api/2.0/tags?after=`, "0", ["name"]));
   console.log(tags);
   const products = (await getData(`https://kidsliving.vendhq.com/api/2.0/products?after=`, "0", [
     "active",
     "id"
   ])).filter(({ active }) => active);
-  console.log(products);
-  const consignments = (await getData(`https://kidsliving.vendhq.com/api/2.0/consignments?after=`, "0")).filter(({ status }) => status !== "RECEIVED");
-  console.log(consignments.length);
-  const sales = (await getData(`https://kidsliving.vendhq.com/api/2.0/sales?after=`, "0"));
+  console.log(products); */
+  /* const consignments = (await getData(`https://kidsliving.vendhq.com/api/2.0/consignments?page_size=50000&after=`, "0")).filter(({ status }) => status === "OPEN");
+  console.log(consignments.length); */
+  const sales = (await getData(`https://kidsliving.vendhq.com/api/2.0/sales?page_size=30000&after=`, "0", [
+    "id",
+    "status",
+    "updated_at"
+  ])).filter(({ updated_at }) => new Date(updated_at) > date);
+  
   console.log(sales.length);
-  res.status(200).send(/* JSON.stringify(data, null, 4) */[products.length, tags.length, consignments.length, sales.length]);
+  res.status(200).send(/* JSON.stringify(data, null, 4) */[/* products.length, tags.length, */ /* consignments.length, */  sales.length]);
 }
