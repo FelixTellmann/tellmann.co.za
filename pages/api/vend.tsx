92dd1eab235b8635e539f4cd77299cfaa0197357
FelixTellmann <felix@tellmann.co.za>
2020-12-23 10:50:51 +0200

    Added Blog Post

--- pages/api/vend.tsx
+++ pages/api/vend.tsx
@@ -1,9 +1,10 @@
-import crypto from "crypto-js";
+import Base64 from "crypto-js/enc-base64";
+import sha256 from "crypto-js/hmac-sha512";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req);
   console.log(req.headers);
-  console.log(crypto.createHmac("sha256", "8fac48e9737d41c5b9aa699fb00ae2e8"));
+  console.log(Base64.stringify(sha256("sha256", `${JSON.stringify(req.body)}8fac48e9737d41c5b9aa699fb00ae2e8`)));
   res.status(200).send("great!");
 }
