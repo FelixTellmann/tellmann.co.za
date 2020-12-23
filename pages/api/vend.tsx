dd48beff4298ec172c2d11a6290ad0faf0aedbda
FelixTellmann <felix@tellmann.co.za>
2020-12-23 09:31:03 +0200

    Added Blog Post

--- pages/api/vend.tsx
+++ pages/api/vend.tsx
@@ -1,7 +1,9 @@
+import crypto from "crypto-js";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req);
   console.log(req.headers);
+  console.log(crypto.createHmac("sha256", "8fac48e9737d41c5b9aa699fb00ae2e8"));
   res.status(200).send("great!");
 }
