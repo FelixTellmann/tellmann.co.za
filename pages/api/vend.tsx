7ce1bbf9d8bc6b5fe88dc11a43a959b84de81d6d
FelixTellmann <felix@tellmann.co.za>
2020-12-23 09:13:42 +0200

    Added Blog Post

--- pages/api/vend.tsx
+++ pages/api/vend.tsx
@@ -2,5 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req);
+  console.log(req.headers);
   res.status(200).send("great!");
 }
