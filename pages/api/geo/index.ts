b0306e980275d398da08e8f670153d676b158142
Felix <felix@tellmann.co.za>
2021-11-15 15:02:13 +0200

    Commit from Office 2021-10

--- pages/api/geo/index.ts
+++ pages/api/geo/index.ts
@@ -6,7 +6,7 @@ type IndexFunction = (req: NextApiRequest, res: NextApiResponse) => Promise<void
 export const Index: IndexFunction = async (req, res) => {
   console.log(req);
   console.log(JSON.stringify(req.headers));
-  res.status(200).send("asdasd");
+  res.status(200).json(req.headers["x-vercel-ip-country"]);
 };
 
 export default allowCors(Index);
